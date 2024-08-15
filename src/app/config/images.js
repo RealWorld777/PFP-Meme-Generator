f3c6538fe7ccd7bf10905f583c7284f4c28de1e6
RealWorld777 <discord.dream120@gmail.com>
2024-08-15 22:31:55 +0500

    colour selector done

---
+++ src/app/config/images.js
@@ -0,0 +1,25 @@
+import head1 from "../../app/assets/HEAD_Rick_var1.png"
+import head2 from "../../app/assets/HEAD_Cfb_var0.png"
+import head3 from "../../app/assets/HEAD_King_var0.png"
+import head4 from "../../app/assets/HEAD_Logan_var0.png"
+import body1 from "../../app/assets/BODY_Cfb_var0.png"
+import body2 from "../../app/assets/BODY_Cfb_var1.png"
+import body3 from "../../app/assets/BODY_King_var1.png"
+import body4 from "../../app/assets/BODY_King_var0.png"
+import body5 from "../../app/assets/BODY_Logan_var0.png"
+import rick from "../../app/assets/rick.png"
+
+const images = {
+    head1,
+    head2,
+    head3,
+    head4,
+    body1,
+    body2,
+    body3,
+    body4,
+    body5,
+    rick
+}
+
+export default images;
--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,17 @@
 
 "use client";
 import Image from "next/image";
-import { useEffect, useRef } from "react";
-import back from "../app/assets/BACK_Cfb.png"
-import head from "../app/assets/HEAD_Rick_var1.png"
-import body from "../app/assets/BODY_Cfb_var0.png"
-import rick from "../app/assets/rick.png"
+import { useEffect, useRef, useState } from "react";
 import html2canvas from "html2canvas";
-import { AlphaPicker, SketchPicker } from "react-color";
+import { HexColorPicker, HexColorInput } from "react-colorful";
+import images from "./config/images";
+
+
 export default function Home() {
 
   const captureRef = useRef(null);
-
+  const [color, setColor] = useState("#aabbcc");
+  const [tab,setTab] = useState("background")
   const captureImage = () => {
     html2canvas(captureRef.current).then((canvas) => {
       const link = document.createElement('a');
@@ -21,10 +21,52 @@ export default function Home() {
     });
   };
 
+  const bodiesData = [
+    {
+      image: images.body1,
+      name: "white T-shirt 1"
+    },
+    {
+      image: images.body2,
+      name: "white T-shirt 2"
+    },
+    {
+      image: images.body3,
+      name: "King 1"
+    },
+    {
+      image: images.body4,
+      name: "King 2"
+    },
+    {
+      image: images.body5,
+      name: "Logan"
+    },
+  ]
+
+  const headsData = [
+    {
+      image: images.head1,
+      name: "Rick"
+    },
+    {
+      image: images.head2,
+      name: "Normal Head"
+    },
+    {
+      image: images.head3,
+      name: "King"
+    },
+    {
+      image: images.head4,
+      name: "Logan"
+    },
+
+  ]
 
   return (
     <div className="bg-[#E1E1E1] flex  justify-center ">
-      
+
       <div className="w-[100%]  flex flex-col    ">
 
 
@@ -40,7 +82,7 @@ export default function Home() {
 
         <div className="flex w-[100%] border-t-2 border-b-2 border-black">
           <div className="w-[50%] border-r-2 border-black">
-            <Image src={rick} className="w-[100%]" />
+            <Image src={images.rick} className="w-[100%]" />
           </div>
           <div className="flex flex-col justify-between px-12 py-8 w-1/2 bg-white">
             <div>
@@ -58,28 +100,58 @@ export default function Home() {
 
           <div className="w-[60%] border-2 border-black bg-white">
             <div className="grid grid-cols-3 bricolageSemibold text-3xl border-b-2 border-black">
-              <div className="p-4 text-center border-r-2 border-black">
+              <div className="p-4 text-center border-r-2 border-black cursor-pointer hover:bg-[#FF6B00] transition duration-200" onClick={()=>{
+                setTab("background")
+              }}>
                 BACKGROUND
               </div>
-              <div className="p-4 text-center border-r-2 border-black">
+              <div className="p-4 text-center border-r-2 border-black cursor-pointer hover:bg-[#FF6B00] transition duration-200" onClick={()=>{
+                setTab("head")
+              }}>
                 HEAD
               </div>
-              <div className="p-4 text-center">
+              <div className="p-4 text-center cursor-pointer hover:bg-[#FF6B00] transition duration-200" onClick={()=>{
+                setTab("body")
+              }}>
                 BODY
               </div>
             </div>
 
-            <SketchPicker />
+            <div className="p-12">
+
+              {tab === "background" &&
+              <>
+              <HexColorPicker color={color} onChange={setColor} />
+              <HexColorInput color={color} onChange={setColor} />
+              </>
+              }
+              {
+                tab === "head" && 
+                <>
+                <div>
+                  head
+                </div>
+                </>
+              }
+              {
+                tab === "body" && 
+                <>
+                <div>
+                  body
+                </div>
+                </>
+              }
+            </div>
           </div>
 
 
           <div className=" w-[30%]">
-<div ref={captureRef} style={{ position: 'relative', width: '250px', height: '250px' }}>
-        <Image src={back} style={{ position: 'absolute', top: 0, left: 0 }} />
-        <Image src={body} style={{ position: 'absolute', top: 0, left: 0 }} />
-        <Image src={head} style={{ position: 'absolute', top: 0, left: 0 }} />
-      </div>
-      <button onClick={captureImage}>Download Image</button>
+            <div ref={captureRef} style={{ position: 'relative', width: '250px', height: '250px', backgroundColor: color }}>
+              
+              <Image src={images.body1} style={{ position: 'absolute', top: 0, left: 0 }} />
+              <Image src={images.head1} style={{ position: 'absolute', top: 0, left: 0 }} />
+            </div>
+            <button onClick={captureImage}>Download Image</button>
           </div>
 
 
@@ -99,7 +171,7 @@ export default function Home() {
           </div>
 
           <div className=" bg-white p-12 bricolageSemibold text-2xl border-t-2 border-black">
-          IllEGAL MEMES © 2024 - ALL RIGHTS ARE RESERVED.
+            IllEGAL MEMES © 2024 - ALL RIGHTS ARE RESERVED.
           </div>
         </footer>
       </div>
