08dbe7f8c6537f340e4d4b5de3d4e7496f1512cd
RealWorld777 <discord.dream120@gmail.com>
2024-08-22 21:55:50 +0500

    header bug fixed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import { ReactNode } from 'react';
 
 interface LayoutProps {
@@ -11,24 +12,24 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
         {/* Add any global metadata or styles here */}
         <title>My Next.js App</title>
       </head>
-      <body className='relative'>
-        
-        <nav className="w-[100%] absolute flex justify-between p-4 bg-[#E1E1E1]">
-          <h2 className="text-2xl bricolageSemibold text-black">
-            MEME GENERATOR
-          </h2>
-          <ul className='flex '>
-            <li>
-              <a href="/">Home</a>
-            </li>
-            <li>
-              <a href="/about">About</a>
-            </li>
-            
-          </ul>
-          <h2 className="text-xl workSans text-black">
-            about
-          </h2>
+      <body className=''>
+        <nav className='relative h-[64px] bg-[#E1E1E1]'>
+          <div className="w-[100%] absolute flex justify-between p-4 bg-[#E1E1E1]">
+            <h2 className="text-2xl bricolageSemibold text-black">
+              MEME GENERATOR
+            </h2>
+            <ul className='flex relative right-[2%] gap-3 '>
+              <li className='hidden sm:block workSans'>
+                <Link href="/">Home</Link>
+              </li>
+              <li className='hidden sm:block workSans'>
+                <Link href="/about">About</Link>
+              </li>
+            </ul>
+            <h2 className="hidden sm:block text-xl workSans text-black">
+              <Link href="/about">About</Link>
+            </h2>
+          </div>
         </nav>
         <main>{children}</main>
       </body>
