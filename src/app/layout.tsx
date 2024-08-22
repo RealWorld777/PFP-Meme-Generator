90ba90766026015ca0f40657276bf1bf9af1ef5f
RealWorld777 <discord.dream120@gmail.com>
2024-08-22 22:46:49 +0500

    responsive header with burger menu done

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,16 @@
+"use client";
 import Link from 'next/link';
-import { ReactNode } from 'react';
-
+import { ReactNode, useState } from 'react';
+import burgerBtn from "./assets/more.png"
+import Image from 'next/image';
 interface LayoutProps {
   children: ReactNode;
 }
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
+
+  const [menuOpen, setMenuOpen] = useState(false)
+
   return (
     <html lang="en">
       <head>
@@ -29,9 +34,24 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
             <h2 className="hidden sm:block text-xl workSans text-black">
               <Link href="/about">About</Link>
             </h2>
+            <div className='w-5 h-5 block sm:hidden cursor-pointer' onClick={()=>{
+              setMenuOpen(!menuOpen)
+            }}>
+              <Image alt="burgerBtn" src={burgerBtn} />
+            </div>
           </div>
         </nav>
         <main>{children}</main>
+        {
+          menuOpen &&
+
+          <div className='absolute bg-[#E1E1E1] w-[100vw] top-[66px] p-6 flex flex-col items-center border-b-2 workSans border-black'>
+            <Link href="/">Home</Link>
+
+            <Link href="/about">About</Link>
+
+          </div>
+        }
       </body>
     </html>
   );
