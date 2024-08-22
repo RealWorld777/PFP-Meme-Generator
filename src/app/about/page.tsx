10f6d75e9d7ec210059c79e983ac4121dac6a683
RealWorld777 <discord.dream120@gmail.com>
2024-08-22 19:42:04 +0500

    navigation functionality fixed on local

---
+++ src/app/about/page.tsx
@@ -0,0 +1,15 @@
+import Layout from "../layout";
+import "../globals.css"
+const About: React.FC = () => {
+    return (
+        <Layout >
+
+            <div>
+                <h1>About Page</h1>
+                <p>This is the about page.</p>
+            </div>
+        </Layout>
+    );
+};
+
+export default About;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,8 @@ import { ref, listAll, getDownloadURL } from 'firebase/storage';
 import defaultBody from "../app/assets/BODY_Cfb_var1.png";
 import defaultHead from "./assets/HEAD_Cfb_var0.png"
 import mainImg from "./assets/Frame5.svg"
-
+import Layout from "./layout";
+import './globals.css';
 export default function Home() {
 
   const captureRef = useRef<HTMLDivElement>(null);
@@ -138,194 +139,196 @@ export default function Home() {
   }, []);
 
   return (
-    <div className="bg-[#E1E1E1] flex  justify-center ">
+    <Layout>
+      <div className="bg-[#E1E1E1] flex  justify-center ">
 
-      <div className="w-[100%]  flex flex-col    ">
-
-
-        <nav className="w-[100%] flex justify-between p-4">
-          <h2 className="text-2xl bricolageSemibold text-black">
-            MEME GENERATOR
-          </h2>
-          <h2 className="text-xl workSans text-black">
-            about
-          </h2>
-        </nav>
+        <div className="w-[100%]  flex flex-col    ">
 
 
-        <div className="sm:flex w-[100%]  border-t-2 border-b-2 border-black">
-          <div className="w-[100%] sm:w-[50%] border-b-2 sm:border-b-0  sm:border-r-2 border-black">
-            <Image alt="" src={mainImg} className="w-[100%]" />
-          </div>
-          <div className=" flex flex-col justify-between px-7 py-5 md:px-12 md:py-8 w-[100%] sm:w-1/2 bg-white">
-            <div>
-
-              <div className="bricolageSemibold text-3xl md:text-5xl lg:text-7xl text-black">CREATE & DOWNLOAD YOUR PUNKS 12PX PFP!</div>
-              <div className="mt-3 text-base md:text-xl lg:text-2xl workSans text-black">Pick and choose between various elements to compose your Punk 12px PFP</div>
-            </div>  
-            <div className="bricolageSemibold text-xl md:text-2xl lg:text-4xl text-black ">Read <span className=" text-[#FF6B00] underline cursor-pointer">instructions</span>  for more info</div>
-          </div>
-        </div>
+          {/* <nav className="w-[100%] flex justify-between p-4">
+            <h2 className="text-2xl bricolageSemibold text-black">
+              MEME GENERATOR
+            </h2>
+            <h2 className="text-xl workSans text-black">
+              about
+            </h2>
+          </nav> */}
 
 
-
-        <div className="lg:flex p-6 lg:p-12 justify-between">
-
-          <div className="w-full mb-5 lg:mb-0 lg:w-[60%] border-2 border-black bg-white ">
-            <div className="grid grid-cols-4 sm:grid-cols-3 bricolageSemibold text-xl md:text-3xl border-b-2 border-black">
-              <div className="col-span-2 sm:col-span-1 p-4 text-center  border-r-2 border-black cursor-pointer hover:bg-[#FF6B00] transition duration-200" onClick={() => {
-                setTab("background")
-              }}>
-                BACKGROUND
-              </div>
-              <div className="p-4 text-center border-r-2 border-black cursor-pointer hover:bg-[#FF6B00] transition duration-200" onClick={() => {
-                setTab("head")
-              }}>
-                HEAD
-              </div>
-              <div className="p-4 text-center cursor-pointer hover:bg-[#FF6B00] transition duration-200" onClick={() => {
-                setTab("body")
-              }}>
-                BODY
-              </div>
+          <div className="sm:flex w-[100%]  border-t-2 border-b-2 border-black">
+            <div className="w-[100%] sm:w-[50%] border-b-2 sm:border-b-0  sm:border-r-2 border-black">
+              <Image alt="" src={mainImg} className="w-[100%]" />
             </div>
+            <div className=" flex flex-col justify-between px-7 py-5 md:px-12 md:py-8 w-[100%] sm:w-1/2 bg-white">
+              <div>
 
-            <div className="p-12 h-[90%] flex flex-col justify-between">
+                <div className="bricolageSemibold text-3xl md:text-5xl lg:text-7xl text-black">CREATE & DOWNLOAD YOUR PUNKS 12PX PFP!</div>
+                <div className="mt-3 text-base md:text-xl lg:text-2xl workSans text-black">Pick and choose between various elements to compose your Punk 12px PFP</div>
+              </div>
+              <div className="bricolageSemibold text-xl md:text-2xl lg:text-4xl text-black ">Read <span className=" text-[#FF6B00] underline cursor-pointer">instructions</span>  for more info</div>
+            </div>
+          </div>
 
-              {tab === "background" &&
-                <div className="sm:flex ">
-                  <div className="mr-10 mb-5 sm:mb-0">
 
-                    <HexColorPicker color={color} onChange={setColor} />
-                    <HexColorInput color={color} onChange={setColor} />
 
-                  </div>
+          <div className="lg:flex p-6 lg:p-12 justify-between">
 
-                  <div className=" flex flex-wrap gap-2">
-                    {hexColors.map(color => (
-                      <div
-                        key={color}
-                        style={{ backgroundColor: color }}
-                        className="w-16 h-16 border-2 border-black cursor-pointer"
-                        onClick={() => setColor(color)}
-                      ></div>
-                    ))}
-                  </div>
-
-                </div>
-              }
-              {
-                tab === "head" &&
-                <div>
-                  <div className="workSans text-2xl mb-5 text-black">
-                    Select Head
-                  </div>
-
-                  <div className="flex flex-wrap gap-3">
-                    {headsImages.map((url, index) => (
-                      <div key={index} className="border-2 border-black"
-                        onClick={() => {
-                          setselectedHead(url)
-                        }}
-                      >
-                        <img key={index} src={url} alt={`Image ${index}`} style={{ width: '150px', height: '150px' }} />
-                      </div>
-                    ))}
-                  </div>
-
-                </div>
-              }
-              {
-                tab === "body" &&
-                <div>
-                  <div className="workSans text-2xl mb-5 text-black">
-                    Select Body
-                  </div>
-
-                  <div className="flex flex-wrap gap-3">
-                    {bodiesImages.map((url, index) => (
-                      <div key={index} className="border-2 border-black"
-                        onClick={() => {
-                          setselectedBody(url)
-                        }}
-                      >
-                        <img key={index} src={url} alt={`Image ${index}`} style={{ width: '150px', height: '150px' }} />
-                      </div>
-                    ))}
-                  </div>
-                </div>
-              }
-
-              <div className="mt-5 grid grid-cols-2 bricolageSemibold gap-5 ">
-                <div className="border-2 border-black text-black text-xl sm:text-3xl text-center py-3 cursor-pointer hover:bg-[#FF6B00] transition duration-200" onClick={() => {
-                  getRandomImageAndColor(headsImages, bodiesImages);
+            <div className="w-full mb-5 lg:mb-0 lg:w-[60%] border-2 border-black bg-white ">
+              <div className="grid grid-cols-4 sm:grid-cols-3 bricolageSemibold text-xl md:text-3xl border-b-2 border-black">
+                <div className="col-span-2 sm:col-span-1 p-4 text-center  border-r-2 border-black cursor-pointer hover:bg-[#FF6B00] transition duration-200" onClick={() => {
+                  setTab("background")
                 }}>
-                  SHUFFLE
+                  BACKGROUND
                 </div>
-                <div className="border-2 border-black text-black text-xl sm:text-3xl text-center py-3 cursor-pointer hover:bg-[#FF6B00] transition duration-200" onClick={captureImage}>
-                  DOWNLOAD
+                <div className="p-4 text-center border-r-2 border-black cursor-pointer hover:bg-[#FF6B00] transition duration-200" onClick={() => {
+                  setTab("head")
+                }}>
+                  HEAD
+                </div>
+                <div className="p-4 text-center cursor-pointer hover:bg-[#FF6B00] transition duration-200" onClick={() => {
+                  setTab("body")
+                }}>
+                  BODY
+                </div>
+              </div>
+
+              <div className="p-12 h-[90%] flex flex-col justify-between">
+
+                {tab === "background" &&
+                  <div className="sm:flex ">
+                    <div className="mr-10 mb-5 sm:mb-0">
+
+                      <HexColorPicker color={color} onChange={setColor} />
+                      <HexColorInput color={color} onChange={setColor} />
+
+                    </div>
+
+                    <div className=" flex flex-wrap gap-2">
+                      {hexColors.map(color => (
+                        <div
+                          key={color}
+                          style={{ backgroundColor: color }}
+                          className="w-16 h-16 border-2 border-black cursor-pointer"
+                          onClick={() => setColor(color)}
+                        ></div>
+                      ))}
+                    </div>
+
+                  </div>
+                }
+                {
+                  tab === "head" &&
+                  <div>
+                    <div className="workSans text-2xl mb-5 text-black">
+                      Select Head
+                    </div>
+
+                    <div className="flex flex-wrap gap-3">
+                      {headsImages.map((url, index) => (
+                        <div key={index} className="border-2 border-black"
+                          onClick={() => {
+                            setselectedHead(url)
+                          }}
+                        >
+                          <img key={index} src={url} alt={`Image ${index}`} style={{ width: '150px', height: '150px' }} />
+                        </div>
+                      ))}
+                    </div>
+
+                  </div>
+                }
+                {
+                  tab === "body" &&
+                  <div>
+                    <div className="workSans text-2xl mb-5 text-black">
+                      Select Body
+                    </div>
+
+                    <div className="flex flex-wrap gap-3">
+                      {bodiesImages.map((url, index) => (
+                        <div key={index} className="border-2 border-black"
+                          onClick={() => {
+                            setselectedBody(url)
+                          }}
+                        >
+                          <img key={index} src={url} alt={`Image ${index}`} style={{ width: '150px', height: '150px' }} />
+                        </div>
+                      ))}
+                    </div>
+                  </div>
+                }
+
+                <div className="mt-5 grid grid-cols-2 bricolageSemibold gap-5 ">
+                  <div className="border-2 border-black text-black text-xl sm:text-3xl text-center py-3 cursor-pointer hover:bg-[#FF6B00] transition duration-200" onClick={() => {
+                    getRandomImageAndColor(headsImages, bodiesImages);
+                  }}>
+                    SHUFFLE
+                  </div>
+                  <div className="border-2 border-black text-black text-xl sm:text-3xl text-center py-3 cursor-pointer hover:bg-[#FF6B00] transition duration-200" onClick={captureImage}>
+                    DOWNLOAD
+                  </div>
                 </div>
               </div>
             </div>
+
+
+            <div className=" w-[30%] min-h-[400px]">
+              <div ref={captureRef} className="border-2 border-black" style={{ position: 'relative', width: '320px', height: '320px', backgroundColor: color }}>
+                <Image
+                  alt=""
+                  src={selectedBody}
+                  style={{ position: 'absolute', top: 0, left: 0 }}
+                  width={350} // Add width here
+                  height={350} // Add height here
+                />
+                <Image
+                  alt=""
+                  src={selectedHead}
+                  style={{ position: 'absolute', top: 0, left: 0 }}
+                  width={350} // Add width here
+                  height={350} // Add height here
+                />
+              </div>
+
+              <div className=" w-[320px]">
+                <div className=" bricolageSemibold text-4xl mt-5 text-black">
+                  METADATA
+                </div>
+                <div className="text-xl bg-white border-2 text-black border-dashed workSans border-black py-5 px-2 mt-2">
+                  background: {color}
+                </div>
+                <div className="text-xl bg-white border-2 border-dashed text-black workSans border-black py-5 px-2 mt-2">
+                  head: {extractMetadata(selectedHead)}
+                </div>
+                <div className="text-xl bg-white border-2 border-dashed text-black workSans border-black py-5 px-2 mt-2">
+                  body: {extractMetadata(selectedBody)}
+                </div>
+              </div>
+            </div>
+
+
           </div>
 
-
-          <div className=" w-[30%] min-h-[400px]">
-            <div ref={captureRef} className="border-2 border-black" style={{ position: 'relative', width: '320px', height: '320px', backgroundColor: color }}>
-              <Image
-                alt=""
-                src={selectedBody}
-                style={{ position: 'absolute', top: 0, left: 0 }}
-                width={350} // Add width here
-                height={350} // Add height here
-              />
-              <Image
-                alt=""
-                src={selectedHead}
-                style={{ position: 'absolute', top: 0, left: 0 }}
-                width={350} // Add width here
-                height={350} // Add height here
-              />
-            </div>
-
-            <div className=" w-[320px]">
-              <div className=" bricolageSemibold text-4xl mt-5 text-black">
-                METADATA
+          <footer className="border-t-2 border-black">
+            <div className="p-12 bg-[#FF6B00]" id="aboutus">
+              <div className="bricolageSemibold text-6xl text-black">
+                ABOUT US
               </div>
-              <div className="text-xl bg-white border-2 text-black border-dashed workSans border-black py-5 px-2 mt-2">
-                background: {color}
+              <div className="workSans text-2xl mt-4 text-black">
+                Punks 12px are a free self-inscribe (Bitcoin) pixel art collection (Anno 2024) - first is first - with a total of 10 000 Punks (12×12px)
               </div>
-              <div className="text-xl bg-white border-2 border-dashed text-black workSans border-black py-5 px-2 mt-2">
-                head: {extractMetadata(selectedHead)}
-              </div>
-              <div className="text-xl bg-white border-2 border-dashed text-black workSans border-black py-5 px-2 mt-2">
-                body: {extractMetadata(selectedBody)}
+              <div className=" bricolageSemibold bg-black text-white text-2xl py-2 w-52 mt-4 text-center border-b-2 border-black ">
+                LEARN MORE
               </div>
             </div>
-          </div>
 
-
+            <div className=" bg-white p-12 bricolageSemibold text-black text-2xl border-t-2 border-black">
+              IllEGAL MEMES © 2024 - ALL RIGHTS ARE RESERVED.
+            </div>
+          </footer>
         </div>
-
-        <footer className="border-t-2 border-black">
-          <div className="p-12 bg-[#FF6B00]" id="aboutus">
-            <div className="bricolageSemibold text-6xl text-black">
-              ABOUT US
-            </div>
-            <div className="workSans text-2xl mt-4 text-black">
-              Punks 12px are a free self-inscribe (Bitcoin) pixel art collection (Anno 2024) - first is first - with a total of 10 000 Punks (12×12px)
-            </div>
-            <div className=" bricolageSemibold bg-black text-white text-2xl py-2 w-52 mt-4 text-center border-b-2 border-black ">
-              LEARN MORE
-            </div>
-          </div>
-
-          <div className=" bg-white p-12 bricolageSemibold text-black text-2xl border-t-2 border-black">
-            IllEGAL MEMES © 2024 - ALL RIGHTS ARE RESERVED.
-          </div>
-        </footer>
       </div>
-    </div>
+    </Layout>
   );
 }
 
