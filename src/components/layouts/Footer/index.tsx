0c9168175f664732acfad523a8baaf9734315ff7
RealWorld777 <valkyrie.prim@gmail.com>
2024-10-14 10:35:23 +0900

    chore: update footer text

--- src/components/layouts/Footer/index.tsx
+++ src/components/layouts/Footer/index.tsx
@@ -5,11 +5,11 @@ function Footer() {
     <footer className="border-t-2 border-black">
       <div className="p-12 " id="aboutus">
         <h2 className="bricolageSemibold text-6xl ">ABOUT US</h2>
-        <p className="workSans text-2xl mt-4 ">Punks 12px are a free self-inscribe (Bitcoin) pixel art collection (Anno 2024) - first is first - with a total of 10,000 Punks (12×12px)</p>
-        <div className="bricolageSemibold bg-black text-white text-2xl py-2 w-52 mt-4 text-center border-b-2 border-black">LEARN MORE</div>
+        <p className="workSans text-2xl mt-4 ">PFP Meme Generator is a free tool to generate meme PFP with your favorite CFB style.</p>
+        <button className="bricolageSemibold bg-black text-white text-2xl py-2 w-52 mt-4 text-center border-b-2 border-black">LEARN MORE</button>
       </div>
 
-      <div className="p-12 bricolageSemibold  text-2xl border-t-2 border-black">IllEGAL MEMES © 2024 - ALL RIGHTS ARE RESERVED.</div>
+      <div className="p-12 bricolageSemibold  text-2xl border-t-2 border-black">PFP Meme Generator © {new Date().getFullYear()} - ALL RIGHTS ARE RESERVED.</div>
     </footer>
   );
 }
