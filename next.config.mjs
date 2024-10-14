30b9227797b2c9a4cb3f35392566f647450e6298
RealWorld777 <valkyrie.prim@gmail.com>
2024-10-14 10:15:15 +0900

    chore: avatar images

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['firebasestorage.googleapis.com', 'picsum.photos'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.pravatar.cc',
+      },
+    ],
   },
 };
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,15 +15,15 @@ import { LandingProductFeature } from '../components/landing/LandingProductFeatu
 
 const avatarItems = [
   {
-    imageSrc: 'https://picsum.photos/id/64/100/100',
+    imageSrc: `https://i.pravatar.cc/${Math.floor(Math.random() * 1000)}`,
     name: 'John Doe',
   },
   {
-    imageSrc: 'https://picsum.photos/id/65/100/100',
+    imageSrc: `https://i.pravatar.cc/${Math.floor(Math.random() * 1000)}`,
     name: 'Jane Doe',
   },
   {
-    imageSrc: 'https://picsum.photos/id/669/100/100',
+    imageSrc: `https://i.pravatar.cc/${Math.floor(Math.random() * 1000)}`,
     name: 'Alice Doe',
   },
 ];
@@ -55,7 +55,7 @@ function Home() {
 
         {/* <LandingDiscount discountValueText="$350 off" discountDescriptionText="for the first 10 customers (2 left)" /> */}
 
-        <LandingSocialProof className="w-full mt-12" showRating numberOfUsers={99} suffixText="happy users" avatarItems={avatarItems} />
+        <LandingSocialProof className="w-full mt-12" showRating numberOfUsers={99} suffixText="happy users" avatarItems={avatarItems} size="medium" />
       </LandingPrimaryImageCtaSection>
       <LandingMarquee withBackground variant="secondary">
         {Object.values(images).map((image, index) => {
