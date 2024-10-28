6870b20c20676e0ab27e5cb3d86997fdf8b8585f
RealWorld777 <valkyrie.prim@gmail.com>
2024-10-29 03:42:22 +0900

    update: faq content

--- src/app/Faq/page.tsx
+++ src/app/Faq/page.tsx
@@ -24,8 +24,8 @@ const Faq: React.FC = () => {
           {faqData.map((faq, index) => (
             <div key={index} className="flex justify-between p-5 border-2 border-black mb-5">
               <div>
-                <h3 className="bricolageSemibold text-xl sm:text-4xl">{faq.title}</h3>
-                {activeIndex === index && <p className="workSans sm:text-2xl mt-5 faq-content active">{faq.content}</p>}
+                <h3 className="bricolageSemibold text-xl sm:text-4xl" dangerouslySetInnerHTML={{ __html: faq.title }}></h3>
+                {activeIndex === index && <p className="workSans sm:text-2xl mt-5 faq-content active" dangerouslySetInnerHTML={{ __html: faq.content }}></p>}
               </div>
               <div className="w-[50px] h-[50px] sm:w-[25px] sm:h-[25px] cursor-pointer flex items-center justify-center" onClick={() => toggleFaqActive(index)}>
                 {activeIndex === index ? <ChevronUp size={24} /> : <ChevronDown size={24} />}
